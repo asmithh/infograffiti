8e66bd9818d05da02b20f9412b0ca48cede264af
Alyssa Smith <asmithh99@gmail.com>
2021-07-05 18:10:21 -0400

    working code?

--- example.py
+++ example.py
@@ -9,71 +9,108 @@ from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 import pandas
 import praw
+import requests
 
+# TODO: bursty events/events with positive or negative affect
+# land of giants podcast (FAANG)
+# legaladvice wrt big tech
+
+# TODO: exploratory analysis
+# TODO: dossier (frequent users, avg word count, character count, upvote distribution, sentiment, reading ease, stopword usage)
+# avg word length, media usage, outlink domains, user age (avg account age), co-belonging, comment volume/post volume,
+# user trajectories
 
 with open('creds.yaml', 'r') as f:
     creds = yaml.load(f)
+
 print(creds)    
 REDDIT_ID = creds['id']
 REDDIT_SECRET = creds['secret']
 APP_NAME = creds['app_name']
 
-MY_SUBREDDIT = 'gradschool'
-STOPWORDS = stopwords.words("english")
-LEMMATIZER = WordNetLemmatizer()
-
+MY_SUBREDDIT = 'legaladvice'
+SEARCH_TERM = 'facebook|google|amazon|apple|netflix'
 reddit = praw.Reddit(client_id=REDDIT_ID, client_secret=REDDIT_SECRET, user_agent=APP_NAME)
 subreddit = reddit.subreddit(MY_SUBREDDIT)
 
-top_posts = subreddit.top(limit=1000)
-
-subreddit_top_1000_posts_info = []
-for submission in top_posts:
-    d = {
-        'title': submission.title,
-        'id': submission.id,
-        'body': submission.selftext,
-        'created_ts': submission.created,
-        'comments': submission.comments.list()
-    }
-    subreddit_top_1000_posts_info.append(d)
-
-pickle.dump(subreddit_top_1000_posts_info, open('subreddit_top_1k_posts.pkl', 'wb'))
-
 def get_body_wrapper(comment):
     try:
-        print('success')
         return c.body
     except:
-        print('more comments')
         return ''
 
-def remove_stopwords(item):
-    return [LEMMATIZER.lemmatize(i) for i in item if i not in STOPWORDS]
+def get_author_wrapper(comment):
+    try:
+        return c.author
+    except:
+        return '))NULL(('
 
-def do_lda(top_posts, n_topics):
-    text = [d['body'] + ' '.join([get_body_wrapper(c) for c in d['comments']]) for d in top_posts]
-    text = [t.lower().split(' ') for t in text]
-    text = [remove_stopwords(t) for t in text]
+def get_posts_over_last_n_days(reddit_instance, subreddit, search_term, n_days):
+    dt_created = []
+    authors = []
+    num_comments_all = []
+    upvote_ratios = []
+    scores = []
+    titles = []
+    texts = []
+    comments_texts_all = []
+    comments_authors_all = []
+    for d1 in range(2, n_days):
+        d2 = d1 - 1
+        res = requests.get('http://api.pushshift.io/reddit/search/submission/?q={}&subreddit={}&after={}d&before={}d&size=1000'.format(
+            search_term, subreddit, str(d1), str(d2)))
+        res_json = res.json()['data']
+        for item in res_json:
+            permalink = item['permalink']
+            dt_created.append(item['created_utc'])
+            authors.append(item['author'])
+            num_comments_all.append(item['num_comments'])
+            try:
+                upvote_ratios.append(item['upvote_ratio'])
+            except:
+                print('no upvote ratio')
+            scores.append(item['score'])
+            submission = praw.models.Submission(reddit_instance, url='http://reddit.com' + permalink)
+            titles.append(submission.title)
+            texts.append(submission.selftext)
+            comments = submission.comments.list()
+            comments_authors_all.append([get_author_wrapper(c) for c in comments])
+            comments_texts_all.append([get_body_wrapper(c) for c in comments])
+            
+            if len(dt_created) % 500 == 0:
+                d = {
+                    'dt_created': dt_created, 
+                    'authors': authors, 
+                    'num_comments_all': num_comments_all, 
+                    'upvote_ratios': upvote_ratios,
+                    'scores': scores,
+                    'titles': titles,
+                    'texts': texts,
+                    'comment_texts_all': comments_texts_all,
+                    'comment_authors_all': comments_authors_all
+                }
+                pickle.dump(d, open('reddit_info_dict_intermediate_{}.pkl'.format(str(len(dt_created))), 'wb'))
+ 
 
-    dictionary = corpora.Dictionary(text)
-    dictionary.filter_extremes(no_below=0.05, no_above=0.5)
-    corpus = [dictionary.doc2bow(text_item) for text_item in text]
-    lda_model = models.LdaModel(corpus, num_topics=n_topics, alpha='auto', eval_every=5)
-    for idx, topic in lda_model.show_topics(
-        num_topics=n_topics, num_words=20, formatted=False
-    ):
-        print([(dictionary[int(w[0])], w[0]) for w in topic])
-        print()
-    topics_probas = lda_model[corpus]
-    overall_probas = {i: 0 for i in range(n_topics)}
-    for p in topics_probas:
-        for proba in p:
-            overall_probas[proba[0]] = overall_probas[proba[0]] + proba[1]
+    d = {
+         'dt_created': dt_created, 
+         'authors': authors, 
+         'num_comments_all': num_comments_all, 
+         'upvote_ratios': upvote_ratios,
+         'scores': scores,
+         'titles': titles,
+         'texts': texts,
+         'comment_texts_all': comments_texts_all,
+         'comment_authors_all': comments_authors_all
+    }
+    pickle.dump(d, open('reddit_info_dict_final.pkl', 'wb'))
+     
+get_posts_over_last_n_days(reddit, MY_SUBREDDIT, SEARCH_TERM, 365)
 
-    denominator = sum(overall_probas.values())
-    overall_probas = {k: v / denominator for k, v in overall_probas.items()}
-    print(overall_probas)
 
-do_lda(subreddit_top_1000_posts_info, 4)
+
+
+
+
+
 
